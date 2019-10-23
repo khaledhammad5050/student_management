fabaa6df0bf657bf382b80f440c83aac89c5ef76
Khaled Hammad <khaled.hammad5050@gmail.com>
2019-10-24 00:16:42 +0200

    Stopped at Relational Fields

--- models/student.py
+++ models/student.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields
 
 
@@ -22,7 +20,7 @@ class student_student(models.Model):
     name = fields.Char(string='Name')
     active = fields.Boolean(string='Active', default=True, )
     image = fields.Binary(string="Image", )
-    uni_no = fields.Char(string="Ministry University NO.", required=True, copy=False, )
+    uni_no = fields.Char(string="Ministry University NO.", required=False, copy=False, )
     seat_no = fields.Char(string="Seat NO.", copy=False)
     dob = fields.Date(string="Date Of Birth", required=True, )
     age = fields.Integer(string="Age", )
@@ -30,11 +28,30 @@ class student_student(models.Model):
     ldate = fields.Datetime(string="Last Registration Date", )
     gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female'), ],
                               default='male', )
+    results_ids = fields.One2many(comodel_name="schoolresults.details", inverse_name="student_id",
+                                  string="School Results", required=False, )
     regfees = fields.Float(string="Registration Fees", default='0.0', )
     tutfees = fields.Float(string="Tuition Fees", default='0.0', )
     totfees = fields.Float(string="Total Fees", default='0.0', )
     ref_link = fields.Char(string="External Link", required=False, )
     health_issues = fields.Selection(string="Health Issues Details", selection=[('yes', 'Yes'), ('no', 'No'), ],
                                      required=False, default='no', )
-    template = fields.HTML(string="Template", )
+    # template = fields.HTML(string="Template", )
     state = fields.Selection(string="Status", selection=STATE, default='draft', readonly=True, )
+
+
+###################################################################################
+class schoolresults_details(models.Model):
+    _name = 'schoolresults.details'
+    _description = 'students secondary school education results'
+
+    student_id = fields.Many2one(comodel_name="student.student", string="Student", required=False, ondelete="cascade")
+    subject_id = fields.Many2one(comodel_name="schoolresults.subject", string="Subject", required=False, )
+
+###################################################################################
+class schoolresults_subject(models.Model):
+    _name = 'schoolresults.subject'
+    _rec_name = 'name'
+    _description = 'students secondary school subjects'
+
+    name = fields.Char(string='Subject')
